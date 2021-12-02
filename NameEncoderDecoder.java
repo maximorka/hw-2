1117bb02375994be37bd064b9810a8bbcde638c1
maximorka <46453975+maximorka@users.noreply.github.com>
2021-12-02 18:46:07 +0200

    Update NameEncoderDecoder.java

--- NameEncoderDecoder.java
+++ NameEncoderDecoder.java
@@ -1,12 +1,18 @@
 class NameEncoderDecoder{
   
   public String encode(String name){
-    
-    return String("");
+
+    name =name.replace("e","1").replace("u","2").replace("i","3").replace("o","4").replace("a","5");
+    name = "NOTFORYOU" + name+"YESNOTFORYOU";
+    return name;
   }
   
   public String decode(String name){
+    name =  name.replace("YESNOTFORYOU","");
+   name =  name.replace("NOTFORYOU","").replace("YES","");
     
-    return String("");
+   name =  name.replace("1","e").replace("2","u").replace("3","i").replace("4","o").replace("5","a");
+    
+    return name;
   }
 }
