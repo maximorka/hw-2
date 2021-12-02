22d09dd6c99383ef85f2a2f485cec2ba0b20a531
maximorka <46453975+maximorka@users.noreply.github.com>
2021-12-02 18:39:30 +0200

    Create NameEncoderDecoder.java

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,12 @@
+class NameEncoderDecoder{
+  
+  public String encode(String name){
+    
+    return String("");
+  }
+  
+  public String decode(String name){
+    
+    return String("");
+  }
+}
